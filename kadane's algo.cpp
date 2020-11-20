0bf56954720c2b74096c340248d50fca512941aa
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-20 23:13:05 +0530

    11/20/2020

---
+++ kadane's algo.cpp
@@ -0,0 +1,40 @@
+/*
+	kadane's  algo 
+	https://www.geeksforgeeks.org/maximum-contiguous-circular-sum/
+*/
+
+#include<bits/stdc++.h>
+#define fast  ios::sync_wih_stdio(false); cin.tie(0); cout.tie(0)
+typedef long long int ll;
+
+using namespace std;
+
+void get_ans(int a[],int n)
+{
+	
+}
+
+void solve()
+{
+	int n;
+	cin>>n;
+	int a[i];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	get_ans(a,n);
+}
+
+int main()
+{
+	fast;
+	ll t=1;
+	cin>>t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+
+
