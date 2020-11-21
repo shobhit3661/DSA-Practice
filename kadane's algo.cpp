2ba2184833f62510c23b3542a82ee602556d2405
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-21 13:50:46 +0530

    11/20/2020

--- kadane's algo.cpp
+++ kadane's algo.cpp
@@ -4,13 +4,46 @@
 */
 
 #include<bits/stdc++.h>
-#define fast  ios::sync_wih_stdio(false); cin.tie(0); cout.tie(0)
+#define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 typedef long long int ll;
 
 using namespace std;
 
 void get_ans(int a[],int n)
 {
+	//get intial sum
+	int sum=0;
+	for(int i=0;i<n;i++)
+	{
+		sum += a[i];
+	}
+	
+	//now apply simple kadane
+	//to find minimum consecutive subarray sum
+	
+	int min_so_far = a[0];
+	int temp_min = a[0];
+	
+	int max_so_far = a[0];
+	int temp_max = a[0];
+	
+	for(int i=0;i<n;i++)
+	{
+		//for max subAraay sum;
+		temp_max = max(temp_max+a[i],a[i]);
+		max_so_far = max(temp_max,max_so_far);
+		
+		//for min subarray sum;
+		temp_min = min(temp_min+a[i],a[i]);
+		min_so_far = min(temp_min,min_so_far);
+		
+	}
+	if(min_so_far = sum)
+	{
+		cout<<max_so_far<<"\n";
+	}
+	
+	cout<<max(max_so_far,sum-min_so_far);
 	
 }
 
@@ -18,7 +51,7 @@ void solve()
 {
 	int n;
 	cin>>n;
-	int a[i];
+	int a[n];
 	for(int i=0;i<n;i++)
 	{
 		cin>>a[i];
