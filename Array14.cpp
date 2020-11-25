a626b64d5e7911fdd608afb38765076dda46758e
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-25 11:56:19 +0530

    11/24/2020

--- Array14.cpp
+++ Array14.cpp
@@ -9,23 +9,53 @@ typedef long long int ll;
 
 using namespace std;
 
-void get_next(vector<int>& ar,int n)
+void get_next(vector<int>& ar)
 {
+	 if(ar.size()<=1)return;
+	        int n = ar.size();
+	        int i;
+	        for(i=n-2;i>=0;i--)
+            {
+                if(ar[i]<ar[i+1])
+                {
+                    break;
+                }
+            }
+	        int j;
+            if(i<0)
+            {
+            sort(ar.begin(),ar.end());
+            return;
+        }
+        
+	    if(i>=0)
+	    {
+		    j = n-1;
+		    while(ar[j]<=ar[i])j--;
+	    }
 	
+	    swap(ar[i],ar[j]);
+	
+	    reverse(ar.begin()+i+1,ar.end());
 }
 
 void solve()
 {
 	int n;
 	cin>>n;
-	vector<int> ar(n);
-	for(int i=0;i<ar.size();i++)
+	vector<int> ar;
+	for(int i=0;i<n;i++)
 	{
 		int temp;
 		cin>>temp;
 		ar.push_back(temp);
 	}
-	get_next(ar,n);
+	get_next(ar);
+	
+	for(auto it:ar)
+	{
+		cout<<it<<" ";
+	}
 }
 
 int main()
