9f4a8e014906e885cef8a50c64fc268cb7a94883
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-12-01 23:21:48 +0530

    12/1/2020

--- BitwiseORofallElements.cpp
+++ BitwiseORofallElements.cpp
@@ -1,3 +1,9 @@
+/*
+	bitwise or of all elemenets of a all sub array in the given array
+	
+	https://www.geeksforgeeks.org/sum-of-bitwise-or-of-all-subarrays-of-a-given-array-set-2/
+*/
+
 #include<bits/stdc++.h>
 #define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 #define arin(x,n) for(int i=0;i<n;i++)cin>>x[i]
