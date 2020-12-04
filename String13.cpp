902dd5701b07b6dd3a93fb8c238cdaa4d959b0a1
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-12-04 23:53:14 +0530

    12/4/2020

---
+++ String13.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define arin(x,n) for(int i=0;i<n;i++)cin>>x[i]
+#define debug(x,n) for(int i=0;i<n;i++)cout<<x[i]<<" "
+#define pb push_back
+typedef long long int ll;
+
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin>>n;
+	
+	int ar[n];
+	arin(ar,n);
+	
+}	
+
+int main()
+{
+	fast;
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+
+
