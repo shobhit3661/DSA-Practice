4dc6ee58bafc015395697821e50743d37431b331
shobhit sharma <shobhitsharma.3661@gmail.com>
2021-01-23 20:35:55 +0530

    1/23/21

---
+++ matrix1.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define arin(x,n) for(int i=0;i<n;i++)cin>>x[i]
+#define debug(x,n) for(int i=0;i<n;i++)cout<<x[i]<<" "
+#define pb push_back
+typedef long long int ll;
+
+using namespace std;
+
+void solve()
+{
+	int n,m;
+	cin>>n>>m;
+	
+	int ar[n][m];
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			cin>>ar[i][j];
+		}
+	}
+	
+	
+	
+	
+}
+
+int main()
+{
+	fast;
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+
+
